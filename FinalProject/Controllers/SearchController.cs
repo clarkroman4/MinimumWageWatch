7ccb716863c678a1e300f29686857ef0a614a6e9
clarkroman4@gmail.com <clarkroman4@gmail.com>
2020-01-24 16:50:52 -0600

    Add Search Page

---
+++ FinalProject/Controllers/SearchController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProject.Data;
+using FinalProject.Models;
+using FinalProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public SearchController(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult Index(SearchViewModel searchViewModel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        string searchTerm = searchViewModel.ToString();
+        //        List<WageLocation> searchResults = new List<WageLocation>();
+
+        //        if 
+
+
+        //        return View(searchResults);
+        //    }
+
+        //    return View(searchViewModel);
+        //}
+    }
+   
+}
---
+++ FinalProject/ViewModels/SearchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.ViewModels
+{
+    public class SearchViewModel
+    {
+        [Required(ErrorMessage = "Please enter a search term")]
+        [Display(Name="Search Term")]
+        public string SearchTerm { get; set; }
+
+    }
+}
